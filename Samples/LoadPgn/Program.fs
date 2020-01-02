3bac77882c6c8026729963d64cdf08dd315c698c
Phil Brooks <phil.brooks@asr-services.com>
2020-01-02 14:29:33 +0000

    Update Program.fs

--- Samples/LoadPgn/Program.fs
+++ Samples/LoadPgn/Program.fs
@@ -1,7 +1,6 @@
 ﻿// This is s simple sample to load a pgn file
 
-open FsChessPgn
-open FsChess
+open FsChess.Pgn
 
 [<EntryPoint>]
 let main argv =
@@ -9,21 +8,7 @@ let main argv =
         printfn "You need to specify a pgn file, e.g. LoadPgn file.pgn"
         1
     else
-        //test code
-        let board = Board.Start
-
-        let nbd = 
-            board
-            |>Board.PushSAN "e4"
-            |>Board.PushSAN "e5"
-            |>Board.PushSAN "Nc3"
-            |>Board.PushSAN "Nc6"
-
-        let mv3 = "g1e2"|>Move.FromUci nbd
-        let uci3 = mv3|>Move.ToUci
-        let san3 = mv3|>Move.ToSan nbd
         //end
         let pgnfile = argv.[0]
-        let pgn = pgnfile|>Games.ReadFromFile
-        let npgn = pgn|>Games.SetaMoves
+        let pgn = pgnfile|>Games.ReadListFromFile
         0 // return an integer exit code
