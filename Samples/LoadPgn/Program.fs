cbe09e6ee787f7e35c30eb47fafee571cee26a7d
Phil Brooks <phil.brooks@asr-services.com>
2020-01-23 07:52:28 +0000

    Update Program.fs

--- Samples/LoadPgn/Program.fs
+++ Samples/LoadPgn/Program.fs
@@ -7,27 +7,6 @@ open System.IO
 [<EntryPoint>]
 let main argv =
     
-    let fol = @"D:\pgns"
-    //let f1 = System.IO.Path.Combine(fol,"recent.pgn")
-    //let pgn1 = f1|>Games.ReadSeqFromFile
-    //let pgn100000 = pgn1|>Seq.take 100000|>Seq.toList
-    let f100000 = System.IO.Path.Combine(fol,"recent100000.pgn")
-    //pgn100000|>Games.WriteFile f100000
-    //f100000|>Games.CreateIndex
-
-
-    let ipgn100000 = f100000|>Games.ReadIndexListFromFile
-    let indx100000 = f100000|>Games.GetIndex
-
-
-
-    let fen = "rnb1kbnr/pp1q1ppp/4p3/8/2Bp4/5N2/PPPN1PPP/R1BQK2R w KQkq - 2 7"
-    let bd = fen|>Board.FromStr
-    let start = System.DateTime.Now
-    let mgms = ipgn100000|>Games.FastFindBoard bd indx100000
-    let nd = System.DateTime.Now
-    let elapsed = nd-start
-    
     if argv.Length<>1 then 
         printfn "You need to specify a pgn file, e.g. LoadPgn file.pgn"
         1
