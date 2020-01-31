3dc5d2cb7fac8e2e302c46b752bcb741bd1089ec
Phil Brooks <phil.brooks@asr-services.com>
2020-01-31 12:28:16 +0000

    Update Program.fs

--- Storage/Program.fs
+++ Storage/Program.fs
@@ -4,26 +4,26 @@ open Storage
 [<EntryPoint>]
 let main argv =
     let pgn = "recent10000.pgn"
-    //let s0 = DateTime.Now
-    //let igms0 = pgn|>T0.ReadIndexListFromFile
-    //let e0 = DateTime.Now
-    //let el0 = e0-s0
-    //Console.WriteLine("el0: " + el0.ToString())
-    //let s1 = DateTime.Now
-    //pgn|>T1.ReadFromPgn
-    //let e1 = DateTime.Now
-    //let el1 = e1-s1
-    //Console.WriteLine("el1: " + el1.ToString())
-    //let s1 = DateTime.Now
-    //("T1_" + pgn)|>T1.ExportPgn
-    //let e1 = DateTime.Now
-    //let el1 = e1-s1
-    //Console.WriteLine("el1: " + el1.ToString())
-    //let s1 = DateTime.Now
-    //("T1_" + pgn)|>T1.Save
-    //let e1 = DateTime.Now
-    //let el1 = e1-s1
-    //Console.WriteLine("el1: " + el1.ToString())
+    let s0 = DateTime.Now
+    let igms0 = pgn|>T0.ReadIndexListFromFile
+    let e0 = DateTime.Now
+    let el0 = e0-s0
+    Console.WriteLine("el0: " + el0.ToString())
+    let s1 = DateTime.Now
+    pgn|>T1.ReadFromPgn
+    let e1 = DateTime.Now
+    let el1 = e1-s1
+    Console.WriteLine("el1: " + el1.ToString())
+    let s1 = DateTime.Now
+    ("T1_" + pgn)|>T1.ExportPgn
+    let e1 = DateTime.Now
+    let el1 = e1-s1
+    Console.WriteLine("el1: " + el1.ToString())
+    let s1 = DateTime.Now
+    ("T1_" + pgn)|>T1.Save
+    let e1 = DateTime.Now
+    let el1 = e1-s1
+    Console.WriteLine("el1: " + el1.ToString())
     let s1 = DateTime.Now
     ("T1_" + pgn)|>T1.Load
     let e1 = DateTime.Now
