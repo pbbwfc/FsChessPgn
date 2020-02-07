2a2533e3a3d38f8e3fb1ceebad98ac645e75bf23
Phil Brooks <phil.brooks@asr-services.com>
2020-02-07 20:09:12 +0000

    started pgn change

--- OlmLib/Stats.fs
+++ OlmLib/Stats.fs
@@ -7,30 +7,30 @@ module Stats =
             if cigmmvl.IsEmpty then bdst
             else
                 let hdr,mv = cigmmvl.Head
-                let result = hdr.Result|>GameResult.Parse
+                let result = hdr.Result|>GmResult.Parse
                 let cmvstl = bdst.Mvstats|>List.filter(fun mvst -> mvst.Mv=mv)
                 if cmvstl.IsEmpty then 
                     let mvst = {Mv=mv;Mvstr=mv|>FsChess.Move.ToSan bd;Count=1;Pc=0.0;
-                                WhiteWins=(if result=GameResult.WhiteWins then 1 else 0);
-                                Draws=(if result=GameResult.Draw then 1 else 0);
-                                BlackWins=(if result=GameResult.BlackWins then 1 else 0);
+                                WhiteWins=(if result=GmResult.WhiteWins then 1 else 0);
+                                Draws=(if result=GmResult.Draw then 1 else 0);
+                                BlackWins=(if result=GmResult.BlackWins then 1 else 0);
                                 Score=0.0;DrawPc=0.0}
                     createstats cigmmvl.Tail {bdst with TotCount=(bdst.TotCount+1);
-                                                        TotWhiteWins=(if result=GameResult.WhiteWins then bdst.TotWhiteWins+1 else bdst.TotWhiteWins);
-                                                        TotDraws=(if result=GameResult.Draw then bdst.TotDraws+1 else bdst.TotDraws);
-                                                        TotBlackWins=(if result=GameResult.BlackWins then bdst.TotBlackWins+1 else bdst.TotBlackWins);
+                                                        TotWhiteWins=(if result=GmResult.WhiteWins then bdst.TotWhiteWins+1 else bdst.TotWhiteWins);
+                                                        TotDraws=(if result=GmResult.Draw then bdst.TotDraws+1 else bdst.TotDraws);
+                                                        TotBlackWins=(if result=GmResult.BlackWins then bdst.TotBlackWins+1 else bdst.TotBlackWins);
                                                         Mvstats=mvst::bdst.Mvstats}
                 else
                     let cmvst = cmvstl.Head
                     let mvst = {cmvst with Count=(cmvst.Count+1);
-                                           WhiteWins=(if result=GameResult.WhiteWins then cmvst.WhiteWins+1 else cmvst.WhiteWins);
-                                           Draws=(if result=GameResult.Draw then cmvst.Draws+1 else cmvst.Draws);
-                                           BlackWins=(if result=GameResult.BlackWins then cmvst.BlackWins+1 else cmvst.BlackWins)}
+                                           WhiteWins=(if result=GmResult.WhiteWins then cmvst.WhiteWins+1 else cmvst.WhiteWins);
+                                           Draws=(if result=GmResult.Draw then cmvst.Draws+1 else cmvst.Draws);
+                                           BlackWins=(if result=GmResult.BlackWins then cmvst.BlackWins+1 else cmvst.BlackWins)}
                     let nmvstl = bdst.Mvstats|>List.filter(fun mvst -> mvst.Mv<>mv)
                     createstats cigmmvl.Tail {bdst with TotCount=(bdst.TotCount+1);
-                                                        TotWhiteWins=(if result=GameResult.WhiteWins then bdst.TotWhiteWins+1 else bdst.TotWhiteWins);
-                                                        TotDraws=(if result=GameResult.Draw then bdst.TotDraws+1 else bdst.TotDraws);
-                                                        TotBlackWins=(if result=GameResult.BlackWins then bdst.TotBlackWins+1 else bdst.TotBlackWins);
+                                                        TotWhiteWins=(if result=GmResult.WhiteWins then bdst.TotWhiteWins+1 else bdst.TotWhiteWins);
+                                                        TotDraws=(if result=GmResult.Draw then bdst.TotDraws+1 else bdst.TotDraws);
+                                                        TotBlackWins=(if result=GmResult.BlackWins then bdst.TotBlackWins+1 else bdst.TotBlackWins);
                                                         Mvstats=mvst::nmvstl}
         let nbdst = createstats (igms|>List.ofArray) BrdStatsEMP
         let popmvst (mvst:MvStats) =
--- OlmLib/Types.fs
+++ OlmLib/Types.fs
@@ -2,11 +2,34 @@
 
 [<AutoOpen>]
 module Types =
-    type GameResult = 
+    type GmResult = 
         | Draw = 0
         | WhiteWins = 1
         | BlackWins = -1
         | Open = 9
+
+    type Nag =
+        |Null = 0
+        |Good = 1
+        |Poor = 2
+        |VeryGood = 3
+        |VeryPoor = 4
+        |Speculative = 5
+        |Questionable = 6
+        |Even = 10
+        |Wslight = 14
+        |Bslight = 15
+        |Wmoderate = 16
+        |Bmoderate =17
+        |Wdecisive = 18
+        |Bdecisive = 19
+
+    type MvStrEntry =
+        |MvEntry of int * bool * string
+        |CommEntry of string
+        |EndEntry of GmResult
+        |NagEntry of Nag
+        |RAVEntry of MvStrEntry list
 
     type MvStats =
         {
